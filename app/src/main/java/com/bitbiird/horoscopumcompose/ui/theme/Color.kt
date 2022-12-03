7f8fa800469f81093695cfb51b5429cbf2cc7187
jarg-147 <romegijavier@gmail.com>
2022-12-03 12:07:55 +0100

    [App] - Added resources

--- app/src/main/java/com/bitbiird/horoscopumcompose/ui/theme/Color.kt
+++ app/src/main/java/com/bitbiird/horoscopumcompose/ui/theme/Color.kt
@@ -2,7 +2,35 @@ package com.bitbiird.horoscopumcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Default
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+// Black
+
+val black = Color(0xFF000000)
+val black50 = Color(0x80000000)
+val illicitDarkness = Color(0xFF00002c)
+
+// White
+
+val white = Color(0xFFFFFFFF)
+val compactDiscGrey = Color(0xFFCDCDCD)
+
+// Red
+
+val vineyardAutumn = Color(0xFFF04354)
+
+// Orange
+
+val universityOfTennesseeOrange = Color(0xFFf98500)
+
+// Purple
+
+val magicCarpet = Color(0xFF9186BF)
+val swissPlum = Color(0xFF5f4bbc)
+val blueGem = Color(0xFF49398F)
+val benikakehanaPurple = Color(0xFF595275)
