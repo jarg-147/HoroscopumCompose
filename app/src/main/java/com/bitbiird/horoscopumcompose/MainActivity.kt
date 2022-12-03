2b9353fd3a2dad3e89d868858341b778518ef0d7
jarg-147 <romegijavier@gmail.com>
2022-12-03 12:14:40 +0100

    [App] - Added application class and initial DI

--- app/src/main/java/com/bitbiird/horoscopumcompose/MainActivity.kt
+++ app/src/main/java/com/bitbiird/horoscopumcompose/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.bitbiird.horoscopumcompose.navigation.SetUpNavigation
 import com.bitbiird.horoscopumcompose.ui.theme.HoroscopumComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
