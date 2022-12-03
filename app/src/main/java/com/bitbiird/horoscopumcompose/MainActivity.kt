26312adc4da93ae08b7056ef59c9d56e1ac4a649
jarg-147 <romegijavier@gmail.com>
2022-12-03 15:07:25 +0100

    [Splash] - Updated MainActivity

--- app/src/main/java/com/bitbiird/horoscopumcompose/MainActivity.kt
+++ app/src/main/java/com/bitbiird/horoscopumcompose/MainActivity.kt
@@ -3,10 +3,14 @@ package com.bitbiird.horoscopumcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.bitbiird.horoscopumcompose.navigation.SetUpNavigation
 import com.bitbiird.horoscopumcompose.ui.theme.HoroscopumComposeTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,10 +21,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
             HoroscopumComposeTheme {
                 navController = rememberNavController()
                 SetUpNavigation(navController)
+                SetUpStatusBar()
             }
         }
     }
+
+    @Composable
+    private fun SetUpStatusBar() {
+        val systemUiController = rememberSystemUiController()
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent
+        )
+    }
+
 }
