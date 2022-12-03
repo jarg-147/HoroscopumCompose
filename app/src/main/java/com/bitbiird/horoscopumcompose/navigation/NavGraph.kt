1198eb21104d7bd23a66519726da921662997f52
jarg-147 <romegijavier@gmail.com>
2022-12-03 12:06:48 +0100

    [Navigation] - Added navigation

---
+++ app/src/main/java/com/bitbiird/horoscopumcompose/navigation/NavGraph.kt
@@ -0,0 +1,33 @@
+package com.bitbiird.horoscopumcompose.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.bitbiird.horoscopumcompose.util.constants.NavigationConstants.DETAIL_ARGUMENT_KEY
+
+@Composable
+fun SetUpNavigation(navController: NavHostController) {
+    NavHost(
+        navController = navController, startDestination = Screen.Splash.route
+    ) {
+        composable(route = Screen.Splash.route) {
+            //TODO
+        }
+        composable(route = Screen.Home.route) {
+            //TODO
+        }
+        composable(route = Screen.List.route) {
+            //TODO
+        }
+        composable(
+            route = Screen.Detail.route, arguments = listOf(navArgument(name = DETAIL_ARGUMENT_KEY) {
+                type = NavType.IntType
+            })
+        ) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/bitbiird/horoscopumcompose/util/constants/NavigationConstants.kt
@@ -0,0 +1,6 @@
+package com.bitbiird.horoscopumcompose.util.constants
+
+object NavigationConstants {
+
+    const val DETAIL_ARGUMENT_KEY = "DETAIL_ARGUMENT_KEY"
+}
