024868e1c7cd3c80a1d5cec8a043fd93340e331f
jarg-147 <romegijavier@gmail.com>
2022-12-08 00:12:10 +0100

    [Detail] - Updated DI

--- app/src/main/java/com/bitbiird/horoscopumcompose/di/NetworkModule.kt
+++ app/src/main/java/com/bitbiird/horoscopumcompose/di/NetworkModule.kt
@@ -1,9 +1,12 @@
 package com.bitbiird.horoscopumcompose.di
 
+import android.content.Context
 import com.bitbiird.horoscopumcompose.data.network.HoroscopeApiClient
+import com.bitbiird.horoscopumcompose.util.helpers.InternetConnectionHelper
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -35,5 +38,9 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideHoroscopeApiClient(retrofit: Retrofit): HoroscopeApiClient = retrofit.create(HoroscopeApiClient::class.java)
+
+    @Provides
+    @Singleton
+    fun provideInternetConnectionHelper(@ApplicationContext context : Context): InternetConnectionHelper = InternetConnectionHelper(context)
 
 }
